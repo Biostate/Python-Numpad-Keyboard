6bad2db95cbb27b5153606cd86efd9e4b4fb1cd9
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-01-14 01:19:33 +0300

    from events to release method

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,3 @@
-""" import keyboard  # using module keyboard """
 from pynput.keyboard import Key, Listener, Controller
 from pynput import keyboard
 import time
@@ -24,49 +23,50 @@ numpad = {
 }
 
 
-with keyboard.Events() as events:
-    for event in events:
-        key = event.key
-        currentKey = None
+def on_release(key):
+    global lastKeyStrike
+    global lastKeyStrokeTime
+    global lastKey
+    currentKey = None
+    print('{0} release'.format(key))
+    if key == Key.esc:
+        return False
 
-        if event.key == keyboard.Key.esc:
-            break
-        elif event.key == keyboard.Key.backspace:
-            print('backspace pressed')
-        else:
-            if isinstance(event, keyboard.Events.Press):
-                print('Started: {0} pressed'.format(key))
+    if hasattr(key, 'vk') and key.vk != None and 96 <= key.vk <= 105:
+        if not hasattr(key, 'char'):
+            currentKey = str(key.vk - 96)
+    if hasattr(key, 'vk') and key.vk != None and key.vk == 65437:
+        currentKey = "5"
+    elif hasattr(key, 'char') and key.char != None and key.char.isnumeric():
+            if 0 <= int(key.char) <= 9:
+                currentKey = key.char
 
-                """ this is necessary to Windows and Linux differences """
-                if hasattr(key, 'vk') and key.vk != None and 96 <= key.vk <= 105:
-                    currentKey = str(key.vk - 96)
-                elif hasattr(key, 'char') and key.char != None and key.char.isnumeric():
-                    """ This checks for Linux """
-                    if 0 <= int(key.char) <= 9:
-                        currentKey = key.char
+    if currentKey != None:
+        print("You pressed: " + str(currentKey))
+        keyboardController.tap(Key.backspace)
 
-                if currentKey != None:
-                    print("You pressed: " + str(currentKey))
-                    keyboardController.tap(Key.backspace)
+        if time.time() - lastKeyStrokeTime > 0.5:
+            lastKey = None
 
-                    if time.time() - lastKeyStrokeTime > 0.5:
-                        lastKey = None
+        if lastKey == currentKey:
+            print("You pressed the same key")
+            lastKeyStrike += 1
+            keyboardController.tap(Key.backspace)
 
-                    if lastKey == currentKey:
-                        print("You pressed the same key")
-                        lastKeyStrike += 1
-                        keyboardController.tap(Key.backspace)
+        if lastKey != currentKey:
+            print("You pressed a different key")
+            lastKeyStrike = 0
 
-                    if lastKey != currentKey:
-                        print("You pressed a different key")
-                        lastKeyStrike = 0
+        if lastKeyStrike >= len(numpad[currentKey]):
+            lastKeyStrike = 0
 
-                    if lastKeyStrike >= len(numpad[currentKey]):
-                        lastKeyStrike = 0
+        print(numpad[currentKey][lastKeyStrike]);
+        keyboardController.tap(numpad[currentKey][lastKeyStrike])
 
-                    print(numpad[currentKey][lastKeyStrike]);
-                    keyboardController.tap(numpad[currentKey][lastKeyStrike])
+        """ update last key """
+        lastKey = currentKey
+        lastKeyStrokeTime = time.time()
 
-                    """ update last key """
-                    lastKey = currentKey
-                    lastKeyStrokeTime = time.time()
+
+with Listener(on_release=on_release) as listener:
+    listener.join()
